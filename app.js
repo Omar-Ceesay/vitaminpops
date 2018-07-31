0b6930228b32779a24d8db66e4a392a769333573
DESKTOP-JB2RS2I\OmarC <oman531999@gmail>
2018-07-31 14:57:03 -0500

    E-com template added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const ejs = require('ejs');
 const paypal = require('paypal-rest-sdk');
+const qs = require('querystring');
+var path = require('path');
 
 paypal.configure({
   'mode': 'sandbox', //sandbox or live
@@ -11,60 +13,76 @@ paypal.configure({
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => res.render('index'));
 
 app.post('/pay', (req, res) => {
-  var create_payment_json = {
-    "intent": "sale",
-    "payer": {
-        "payment_method": "paypal"
-    },
-    "redirect_urls": {
-        "return_url": "http://localhost:3000/success",
-        "cancel_url": "http://localhost:3000/cancel"
-    },
-    "transactions": [{
-        "item_list": {
-            "items": [{
-                "name": "Vitamin pop",
-                "sku": "001",
-                "price": "29.99",
-                "currency": "USD",
-                "quantity": 1
-            }]
-        },
-        "amount": {
-            "currency": "USD",
-            "total": "29.99"
-        },
-        "description": "The best vitamin popsicle ever."
-    }]
-  };
-
-  paypal.payment.create(create_payment_json, function (error, payment) {
-    if (error) {
-        throw error;
-    } else {
-        for (var i = 0; i < payment.links.length; i++) {
-          if(payment.links[i].rel === 'approval_url'){
-            res.redirect(payment.links[i].href);
-          }
-        }
+  var body = '';
+  req.on('data', function(chunk) {
+    body += chunk;
+  });
+  req.on('end', function() {
+    var data = qs.parse(body);
+    // now you can access `data.price` and `data.password`
+    console.log(data);
+    var quantity = 1;
+    if(data.quantity){
+      quantity = data.quantity;
     }
+    var total = Number(data.price) * quantity;
+    console.log(total+ "\n");
+    var create_payment_json = {
+      "intent": "sale",
+      "payer": {
+          "payment_method": "paypal"
+      },
+      "redirect_urls": {
+          "return_url": "http://localhost:3000/success/"+data.sku+"/"+total.toString(),
+          "cancel_url": "http://localhost:3000/cancel"``
+      },
+      "transactions": [{
+          "item_list": {
+              "items": [{
+                  "name": "Vitamin Pop",
+                  "sku": data.sku,
+                  "price": "29.99",
+                  "currency": "USD",
+                  "quantity": quantity
+              }]
+          },
+          "amount": {
+              "currency": "USD",
+              "total": total
+          },
+          "description": "The best vitamin popsicle ever."
+      }]
+    };
+
+    paypal.payment.create(create_payment_json, function (error, payment) {
+      if (error) {
+          throw error;
+      } else {
+          for (var i = 0; i < payment.links.length; i++) {
+            if(payment.links[i].rel === 'approval_url'){
+              res.redirect(payment.links[i].href);
+            }
+          }
+      }
+    });
   });
 });
 
-app.get('/success', (req, res) => {
+app.get('/success/:itemId/:total', (req, res) => {
   const payerId = req.query.PayerID;
   const paymentId = req.query.paymentId;
-
+  console.log(Number(req.params.total).toFixed(2));
   const execute_payment_json = {
     "payer_id": payerId,
     "transactions": [{
       "amount": {
         "currency": "USD",
-        "total": "29.99"
+        "total": Number(req.params.total).toFixed(2)
       }
     }]
   };
